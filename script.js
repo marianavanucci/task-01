4f953e6ce9f005ac45b5714b936696ca81f59373
MarianaVanucci <marianavanucciandrade@gmail.com>
2021-06-22 22:27:54 -0300

    task solved

---
+++ script.js
@@ -0,0 +1,38 @@
+
+
+// DESAFIO 1
+// =========
+
+// Faça uma função que receba um parâmetro retorne uma string
+// Caso o parâmetro for um número retorne uma string dizendo que isso é um numero
+// Caso o parâmtetro for uma string retorne uma string dizendo que isso é uma string
+
+function mariana() {
+    console.log('Olá!');
+};
+
+// function validateParameter(parameter) {
+//     if (parameter = number) {
+// return console.log("it's a number");
+//     }
+//     else (parameter = string) 
+//  return console.log("it's a string");
+    
+// }
+
+// const validateParameter = validateParameter
+
+function defineElements() {
+    const elemento = elemento
+}
+
+function validateParameter() {
+    const parametros = defineElements();
+    if (parametros = Number)  {
+        return console.log('Isso é um número!');
+    }
+
+    else (parametros = String) 
+        return console.log('Isso é uma String');
+    
+}
