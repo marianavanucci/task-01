d931ff56f0a707e1e4d3ba734eca92a25900f8df
MarianaVanucci <marianavanucciandrade@gmail.com>
2021-06-24 22:44:43 -0300

    validarPessoa

--- script.js
+++ script.js
@@ -9,6 +9,7 @@
 
 function mariana() {
     console.log('Olá!');
+    return 'oie'
 };
 
 // function validateParameter(parameter) {
@@ -26,13 +27,65 @@ function defineElements() {
     const elemento = elemento
 }
 
-function validateParameter() {
-    const parametros = defineElements();
-    if (parametros = Number)  {
-        return console.log('Isso é um número!');
-    }
 
-    else (parametros = String) 
+//escopo 
+
+function validateParameter (pipoca) {
+    if (typeof pipoca == 'number')  {
+        return console.log('Isso é um número!');
+    } else if (typeof pipoca == 'string') {
         return console.log('Isso é uma String');
-    
+    } else if (typeof pipoca == 'function') {
+        return console.log('Isso é uma Função');
+    }
 }
+
+validateParameter('string');
+
+
+
+// uma funcao - 3 parametros 
+// se o 1 for um numero maior q 18 retorna maior de idade
+
+// se for menor de 18 dizer q é menor
+
+function validarPessoa(x,y,z) {
+    const idade = x
+
+    if (idade >= 18){
+        return console.log('pessoa maior de idade')
+    } else { 
+        return console.log('pessoa menor de idade')
+    }
+}
+
+validarPessoa(65);
+validarPessoa(13);
+
+function validarSeEntraNaFesta(x,y,z) {
+    const idade = x
+    const sexo = z
+
+    if (idade >= 18){
+       if (sexo == 'feminino') {
+        return console.log('menina entra na festa e pega 80.')
+       } else if (sexo == 'masculino') {
+        return console.log('menino entra na festa e paga 100')
+       }
+    } else { 
+        return console.log('pessoa independente do sexo, não entra na festa.')
+    }
+}
+
+validarSeEntraNaFesta(35, null,'feminino')
+
+
+/////////////////////
+
+// if (o que avaliar) {
+    // o que fazer se a avaliação for verdadeira 
+// } else if (o que avaliar) {
+    // o que fazer se a avaliação for verdadeira 
+// } else {
+    // o que fazer se a avaliação for verdadeira 
+// }
